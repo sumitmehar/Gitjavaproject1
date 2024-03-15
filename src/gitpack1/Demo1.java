32173b2c6316550df75a5ff5367698d6481891e8
sumitmehar <sumitdreams2020@gmail.com>
2024-03-15 20:41:31 +0530

    first commit

---
+++ src/gitpack1/Demo1.java
@@ -0,0 +1,19 @@
+package gitpack1;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("***********Program Starts**********");
+		
+		int a= 10,b = 20;
+		
+		if (a<b) {
+			System.out.println("b is greater");
+		}else {
+			System.out.println("a is greater");
+		}
+		
+		System.out.println("***********Program Ends**********");
+	}
+
+}
